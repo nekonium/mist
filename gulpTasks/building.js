9c51af1919e404cf335094b865b2f3db27c41ba5
nyatla <nyatla39@gmail.com>
2017-06-21 15:34:49 +0900

    nekonium patch

--- gulpTasks/building.js
+++ gulpTasks/building.js
@@ -12,7 +12,7 @@ const version = require('../package.json').version;
 
 
 const type = options.type;
-const applicationName = (options.wallet) ? 'Ethereum Wallet' : 'Mist';
+const applicationName = (options.wallet) ? 'Nekonium Wallet' : 'Mist';
 
 
 gulp.task('clean-dist', (cb) => {
@@ -107,9 +107,9 @@ gulp.task('build-dist', (cb) => {
         name: applicationName.replace(/\s/, ''),
         productName: applicationName,
         description: applicationName,
-        homepage: 'https://github.com/ethereum/mist',
+        homepage: 'https://github.com/nekonium/mist',
         build: {
-            appId: `com.ethereum.${type}`,
+            appId: `org.nekonium.${type}`,
             asar: true,
             directories: {
                 buildResources: '../build',
